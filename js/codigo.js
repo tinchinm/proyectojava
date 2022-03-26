f269697cb48179776cfc49ccd9b86865c14148c4
Martin Marzialetti <tinchin.m@gmail.com>
2022-03-26 17:48:07 -0300

    Correcciones

--- js/codigo.js
+++ js/codigo.js
@@ -7,22 +7,39 @@ class Producto {
     vender() {
         this.stock = this.stock - 1;
     }
+    agregar() {
+        this.stock = this.stock + 1;
+    }
     sumaIva() {
         this.precio = this.precio * 1.21;
     }
+    descuento() {
+        this.precio = this.precio - (this.precio * 0.10);
+    }
 }
 
-const micro = new Producto("Micro Intel Core i3", "90000", "5");
-const memoria = new Producto("Memoria DDR4 8GB ", "8000", "9");
+const micro = new Producto("Micro Intel Core i3", "90000", parseInt("5"));
+const memoria = new Producto("Memoria DDR4 8GB ", "8000", parseInt("9") );
 
-console.log("El precio de ", micro.nombre, " sin iva es ", micro.precio); 
+
+console.log("El stock actual de ", micro.nombre," es de ", micro.stock, " unidades");
+
+micro.agregar();
+
+console.log("El stock actual de ", micro.nombre," es de ", micro.stock, " unidades");
+
+console.log("El precio de ", micro.nombre, " sin iva es $", micro.precio); 
 
 micro.sumaIva();
 
-console.log("El precio de ", micro.nombre, " con iva es ", micro.precio);
+console.log("El precio de ", micro.nombre, " con iva es $", micro.precio);
 
-console.log("El stock actual de ", memoria.nombre," es ", memoria.stock);
+micro.descuento();
+
+console.log("El precio con descuento de ", micro.nombre, " es $", micro.precio); 
+
+console.log("El stock actual de ", memoria.nombre," es de ", memoria.stock, " unidades");
 
 memoria.vender();
 
-console.log("El stock actual de ", memoria.nombre," es ", memoria.stock);
+console.log("El stock actual de ", memoria.nombre," es de ", memoria.stock, " unidades");
